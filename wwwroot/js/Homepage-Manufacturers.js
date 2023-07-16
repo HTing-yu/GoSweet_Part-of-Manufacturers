c26db3924ae912c49bb52e080bd21b0d2a722e32
HTing-Yu <yu.822511@gmail.com>
2023-07-16 20:36:06 +0800

    CSS重製

---
+++ wwwroot/js/Homepage-Manufacturers.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+
+    //評價估算
+    var Rating = 3997;
+    var NumberTotal = 996;
+    $(".star").children().eq(Math.round(Rating/NumberTotal)-1).prop('checked',true)
+    $("#NumberOrder").text(NumberTotal)
+    
+    //更新日期設定
+    Now = $(".update").text(NowTime())
+}
+
+//#region 現在時間的方法
+function NowTime(){
+    var NowDate = new Date();
+    var Now = `${NowDate.getFullYear()}年${(NowDate.getMonth()+1)<10?'0'+(NowDate.getMonth()+1):(NowDate.getMonth()+1)}月${NowDate.getDate()<10?'0'+NowDate.getDate():NowDate.getDate()}日 ${NowDate.getHours()<10?'0'+NowDate.getHours():NowDate.getHours()}:${NowDate.getMinutes()<10?'0'+NowDate.getMinutes():NowDate.getMinutes()}:${NowDate.getSeconds()<10?'0'+NowDate.getSeconds():NowDate.getSeconds()}`
+    return Now
+}
+//#endregion 回傳字串
+
+//#region 定義重整事件
+function refresh(){
+    let Now = NowTime() 
+    alert(Now)
+    Now = $(".update").text(Now)
+}
+//#endregion 可以可慮跟NowTime整合
+
+
